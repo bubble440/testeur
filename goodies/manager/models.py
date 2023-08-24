f4b4574470179da47e1fc22652c5dd644ded6374
wilfried <wilsaah963@gmail.com>
2023-08-24 21:34:18 +0100

    create product model and template

--- goodies/manager/models.py
+++ goodies/manager/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.fields.CharField(max_length=100)
+    # image = 
+    price = models.fields.IntegerField()
+    description = models.fields.CharField(max_length=1000)
+    category = models.fields.CharField(max_length=20)
--- goodies/manager/views.py
+++ goodies/manager/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def addproduct(request):
+    return request(request,
+        'goodies/add_product.html'
+    )
